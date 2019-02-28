f6fd9257dc485e80b2ccb574f19f093d57359562
KeisSarpong <kofisar@outlook.com>
2019-02-28 14:01:10 +0100

    PROGRAM TEST

---
+++ SU19-Exercises/Galaga-Exercise-1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Galaga_Exercise_1 {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("bøh");
+        }
+    }
+}
